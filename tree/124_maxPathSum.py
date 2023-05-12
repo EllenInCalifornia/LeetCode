2b45a5cdd8842856104d45eb697e23085073c847
Ellen Wang <claire19900125@gmail.com>
2023-05-12 11:39:21 +0800

    [5,4,8,11,null,13,4,7,2,null,null,null,1], wrong 55, expected 48

--- tree/124_maxPathSum.py
+++ tree/124_maxPathSum.py
@@ -8,9 +8,23 @@
 
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            # [-3], error
-            return float("-inf") 
-        right_max = self.maxPathSum(root.right)
-        left_max = self.maxPathSum(root.left)
-        return max(root.val, root.val + right_max, root.val + left_max, right_max, left_max, root.val + right_max + left_max)
+        # 含有current node的最大值
+        # without current node
+        memo = {}
+        with_cur = self.with_curr(root, memo)
+        return max(memo.values())
+
+    def with_curr(self, root, memo):
+
+        if not node:
+            return float("-inf")
+        if node in memo:
+            return memo[node]
+        right_max = self.with_curr(node.right)
+        memo[node.right] = right_max
+        left_max = self.with_curr(node.left)
+        memo[node.left] = left_max
+        memo[node] = max(node.val, node.val + right_max, node.val + left.max, node.val + right_max + left_max)
+        return memo[node]
+        # [1,-2,-3,1,3,-2,null,-1] 
+      
