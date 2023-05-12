8fa4177f071710e1aaaa10837e9b7725a3bd5937
Ellen Wang <claire19900125@gmail.com>
2023-05-12 10:52:49 +0800

    fix first error

--- tree/124_maxPathSum.py
+++ tree/124_maxPathSum.py
@@ -10,7 +10,7 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         if not root:
             # [-3], error
-            return 0 
+            return float("-inf") 
         right_max = self.maxPathSum(root.right)
         left_max = self.maxPathSum(root.left)
         return max(root.val, root.val + right_max, root.val + left_max, right_max, left_max, root.val + right_max + left_max)
