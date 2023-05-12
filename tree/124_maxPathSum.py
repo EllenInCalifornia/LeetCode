30201efef60d71c6e08c34bf820d778c6dc5b142
Ellen Wang <claire19900125@gmail.com>
2023-05-12 10:44:52 +0800

    faulty recur version

---
+++ tree/124_maxPathSum.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0 
+        right_max = self.maxPathSum(root.right)
+        left_max = self.maxPathSum(root.left)
+        return max(root.val, root.val + right_max, root.val + left_max, right_max, left_max, root.val + right_max + left_max)
